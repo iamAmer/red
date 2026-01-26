29dfb1cb90076f5bd14627fb1806d7622af020b8
amer <muhammedamer7810@gmail.com>
2026-01-26 13:17:10 +0200

    Implement basic grok api call

--- src/agent.ts
+++ src/agent.ts
@@ -1,1 +1,24 @@
-console.log('red is alive')
+import Groq from 'groq-sdk'
+import 'dotenv/config'
+
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY })
+
+async function main() {
+  const chatCompletion = await getGroqChatCompletion()
+  // Print the completion returned by the LLM.
+  console.log(chatCompletion.choices[0]?.message?.content || '')
+}
+
+async function getGroqChatCompletion() {
+  return groq.chat.completions.create({
+    messages: [
+      {
+        role: 'user',
+        content: 'tell me the benifits of llms in 10 words',
+      },
+    ],
+    model: 'openai/gpt-oss-20b',
+  })
+}
+
+main();
