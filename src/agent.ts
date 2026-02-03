083c12859a6bfed02e5500e89c94c7f628fd95db
amer <muhammedamer7810@gmail.com>
2026-02-03 22:57:47 +0200

    add a function calling schema

--- src/agent.ts
+++ src/agent.ts
@@ -63,6 +63,7 @@ async function edit_file(
     return true
   }
   try {
+    return await editExistingFile() 
   } catch (error: any) {
     if (error?.code !== 'ENOENT') throw error
 
@@ -184,6 +185,92 @@ async function run_command(
     return [error.message || String(error), 1]
   }
 }
+
+const tools = [
+  {
+    type: 'function',
+    function: {
+      name: 'edit_file',
+      description:
+        'Apply a diff to a file by replacing occurrences of findStr with replaceStr.',
+      parameters: {
+        type: 'object',
+        properties: {
+          filePath: {
+            type: 'string',
+            description: 'The path of the file to modify',
+          },
+          findStr: {
+            type: 'string',
+            description: 'The string to find in the file',
+          },
+          replaceStr: {
+            type: 'string',
+            description: 'The string to replace with',
+          },
+        },
+        required: ['filePath', 'findStr', 'replaceStr'],
+      },
+    },
+  },
+  {
+    type: 'function',
+    function: {
+      name: 'run_command',
+      description: 'Run a shell command and return its output and error code.',
+      parameters: {
+        type: 'object',
+        properties: {
+          command: {
+            type: 'string',
+            description: 'The command to run in the shell',
+          },
+          workingDir: {
+            type: 'string',
+            description:
+              'The working directory to run the command in (optional)',
+          },
+        },
+        required: ['command'],
+      },
+    },
+  },
+  {
+    type: 'function',
+    function: {
+      name: 'list_directory',
+      description: 'List the contents of a directory.',
+      parameters: {
+        type: 'object',
+        properties: {
+          dirPath: {
+            type: 'string',
+            description:
+              'The path to the directory to list. Defaults to the current directory.',
+          },
+        },
+        required: [],
+      },
+    },
+  },
+  {
+    type: 'function',
+    function: {
+      name: 'read_file_content',
+      description: 'Read and return the content of a file.',
+      parameters: {
+        type: 'object',
+        properties: {
+          filePath: {
+            type: 'string',
+            description: 'The path to the file to read.',
+          },
+        },
+        required: ['filePath'],
+      },
+    },
+  },
+]
 
 const groq = new Groq({ apiKey: GROQ_API_KEY })
 async function main() {
